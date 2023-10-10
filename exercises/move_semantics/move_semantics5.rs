730e21d310381697b023892c3c389510065bd32a
lmve <1831232566@qq.com>
2023-10-10 11:35:06 +0800

    day one

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,13 +6,13 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
+// 让我只通过重新排序“main（）”中的行来编译，但不添加，更改或删除其中任何一个。
+//!!!
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut x;
     *z += 1000;
     assert_eq!(x, 1200);
 }
