278a847a0f19ed80f5889871486583c7b3a2b316
lmve <1831232566@qq.com>
2023-10-10 20:46:43 +0800

    Commit twice

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -5,13 +5,16 @@
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
 
-fn current_favorite_color() -> String {
+// fn current_favorite_color() -> String {  //string 版本
+//     "blue".to_string()
+// }
+fn current_favorite_color() -> &'static str{  //字符串切片版本
     "blue"
 }
--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -5,17 +5,17 @@
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {// 形式参数与实际参数要一致
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
     }
 }
 
-fn is_a_color_word(attempt: &str) -> bool {
+fn is_a_color_word(attempt: &str) -> bool {  //attempt 是引用类型
     attempt == "green" || attempt == "blue" || attempt == "red"
 }
